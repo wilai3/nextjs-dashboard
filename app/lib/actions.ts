00c972fccb41ac16927bb7c7d8696b9897d7fc92
wilai3 <wingl9082@gmail.com>
2024-08-27 19:44:37 -0700

    Fixed Small Bugs

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -69,7 +69,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(prevState: State, formData: FormData) {
+export async function updateInvoice( id: string, prevState: State, formData: FormData) {
     const validatedFields = UpdateInvoice.safeParse({
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -22,7 +22,7 @@ export default function EditInvoiceForm({
 
   const initialState: State = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
-  const [state, formAction] = useActionState(updateInvoice, initialState);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
 
   return (
     <form action={formAction}>;
